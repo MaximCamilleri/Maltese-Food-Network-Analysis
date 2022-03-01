42fea65d6cbb20e4316235edd3686299873260af
Max Camilleri <max.camilleri.20@um.edu.mt>
2022-03-01 08:49:35 +0100

    Completed code to add the recipes and links to the database

--- Website Design/PopulatingRecipelinksDB.php
+++ Website Design/PopulatingRecipelinksDB.php
@@ -13,3 +13,18 @@ $connContact = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 if(!$connContact){
     die("Connection failed: ". mysqli_connect_error());
 }
+
+$open = fopen("../CSV/listOfRecipes.csv", "r");
+
+$column = fgetcsv($open, 1000, ",");
+
+while(($column = fgetcsv($open, 1000, ",")) !== FALSE){
+   $sqlInsert = "Insert into recipeLinks (recipeName, recipeLink) VALUES (?, ?)";
+
+    $stmt = $connContact->prepare($sqlInsert);
+    $stmt->bind_param("ss", $column[2], $column[1]);
+    $stmt->execute();   
+
+}
+
+fclose($open);
