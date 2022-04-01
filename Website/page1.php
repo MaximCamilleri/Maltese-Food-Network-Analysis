3fa4e7904df63ced0c1e593806ee071212216cb8
lucaslautier <73827777+lucaslautier@users.noreply.github.com>
2022-04-01 12:50:21 +0200

    page 1 responsiveness

--- Website/page1.php
+++ Website/page1.php
@@ -3,6 +3,7 @@
 <html>
     <head>
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <script src = "https://unpkg.com/split.js/dist/split.min.js"></script>
         <link href="styles.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">
         <link rel= "stylesheet" href = "page1.css">
@@ -28,36 +29,47 @@
                     <div class="container-fluid">
                         <!-- <button class="btn btn-primary" id="sidebarToggle"><i class="fas fa-bars" ></i></button> -->
                         <button class = "btn btn-primary" id = "sidebarToggle">
-                            <i class="fas fa-bars fa-lg"></i>
+                            <i class="fas fa-bars fa-lg"></i>   
                         </button>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                        <!--<div class="collapse navbar-collapse" id="navbarSupportedContent">
-                             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                                <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#!">Action</a>
-                                        <a class="dropdown-item" href="#!">Another action</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#!">Something else here</a>
-                                    </div>
-                                </li>
-                            </ul> 
-                        </div>-->
+                        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button> -->
+                       
                     </div>
                 </nav>
+                <div class = "content">
+                    <div class = "cell text">
+                        <div class = "container-fluid intro">
+
+                            <h1 class = "mt-4">Food Network Analysis</h1>
+
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius placeat labore corrupti perferendis architecto repellat quos magni, impedit ad fugiat ipsum ab, commodi nemo consequatur numquam accusamus fugit expedita ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quidem ea sint corrupti mollitia culpa, quod assumenda recusandae rerum aut perferendis, eum, provident sunt doloribus iure? Ducimus nesciunt perspiciatis cumque!</p>
+                            <button class ="button test-button" type="button">Test it out!</button>
+                        </div> 
 
 
-        <div class = "container-fluid intro">
+                    </div>
+                    <div class = "cell image">
+                        <!-- <img src="food.png" alt="food"> -->
+                    </div>
+                </div>
+                
+                <script> Split(['.text', '.image']); </script>
+                
+              
+                
+                <script src="scripts.js"></script>
 
-            <h1 class = "mt-4">Food Network Analysis</h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius placeat labore corrupti perferendis architecto repellat quos magni, impedit ad fugiat ipsum ab, commodi nemo consequatur numquam accusamus fugit expedita ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quidem ea sint corrupti mollitia culpa, quod assumenda recusandae rerum aut perferendis, eum, provident sunt doloribus iure? Ducimus nesciunt perspiciatis cumque!</p>
-            <button class ="button test-button" type="button">Test it out!</button>
-        </div>
 
-        <script src="scripts.js"></script>
+
+
+        <!-- <div class = "container-fluid intro">
+
+             <h1 class = "mt-4">Food Network Analysis</h1>
+
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius placeat labore corrupti perferendis architecto repellat quos magni, impedit ad fugiat ipsum ab, commodi nemo consequatur numquam accusamus fugit expedita ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quidem ea sint corrupti mollitia culpa, quod assumenda recusandae rerum aut perferendis, eum, provident sunt doloribus iure? Ducimus nesciunt perspiciatis cumque!</p>
+             <button class ="button test-button" type="button">Test it out!</button>
+         </div>  -->
+
+        
     </body>
 </html>
